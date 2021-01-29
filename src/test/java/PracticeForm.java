976678298f8178c697aa324a121f658b55a02d0b
Admin <bogtinys20@gmail.com>
2021-01-29 16:31:09 +0300

    javafaker

--- src/test/java/PracticeForm.java
+++ src/test/java/PracticeForm.java
@@ -1,6 +1,11 @@
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
@@ -16,11 +21,16 @@ public class PracticeForm {
 
     @Test
     void Form() {
-        String firstName = "Dim",
-                lastName = "Dyn",
-                email = "dima@gmail.com",
+        Faker faker = new Faker();
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-GB"), new RandomService());
+
+        String firstName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                email = fakeValuesService.bothify("????##@gmail.com"),
+                mobile = fakeValuesService.regexify("[0-9]{10}"),
+                StateCurrentAddress = faker.address().country(),
+                CityCurrentAddress = faker.address().city(),
                 gender = "Male",
-                mobile = "7999999999",
                 dayOfBirth = "25",
                 monthOfBirth = "January",
                 yearOfBirth = "1995",
@@ -30,51 +40,50 @@ public class PracticeForm {
                 hobby2 = "Reading",
                 hobby3 = "Music",
                 picture = "image.png",
-                currentAddress = "adaa daa dada 11.22",
                 state = "Haryana",
                 city = "Karnal";
 
-    open("https://demoqa.com/automation-practice-form");
+        open("https://demoqa.com/automation-practice-form");
 
-    $("#firstName").setValue(firstName);
-    $("#lastName").setValue(lastName);
-    $("#userEmail").setValue(email);
-    $("#userNumber").setValue(mobile);
-    $("#genterWrapper").$(byText(gender)).click();
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#userNumber").setValue(mobile);
+        $("#genterWrapper").$(byText(gender)).click();
 
-    $("#dateOfBirthInput").click();
-    $(".react-datepicker__year-select").click();
-    $(".react-datepicker__year-select").selectOption(yearOfBirth);
-    $(".react-datepicker__month-select").click();
-    $(".react-datepicker__month-select").selectOption(monthOfBirth);
-    $(".react-datepicker__day--0" + dayOfBirth).click();
-    $("#subjectsInput").val(subject1);
-    $(".subjects-auto-complete__menu").$(byText(subject1)).click();
-    $("#subjectsInput").val(subject2);
-    $(".subjects-auto-complete__menu").$(byText(subject2)).click();
-    $("#hobbiesWrapper").$(byText(hobby1)).click();
-    $("#hobbiesWrapper").$(byText(hobby2)).click();
-    $("#hobbiesWrapper").$(byText(hobby3)).click();
-    $("#uploadPicture").uploadFromClasspath(picture);
-    $("#currentAddress").setValue(currentAddress);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
+        $("#subjectsInput").val(subject1);
+        $(".subjects-auto-complete__menu").$(byText(subject1)).click();
+        $("#subjectsInput").val(subject2);
+        $(".subjects-auto-complete__menu").$(byText(subject2)).click();
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#hobbiesWrapper").$(byText(hobby2)).click();
+        $("#hobbiesWrapper").$(byText(hobby3)).click();
+        $("#uploadPicture").uploadFromClasspath(picture);
+        $("#currentAddress").setValue(StateCurrentAddress + CityCurrentAddress);
 
-    $("#state").click();
-    $("#state").$(byText(state)).click();
-    $("#city").click();
-    $("#city").$(byText(city)).click();
-    $("#submit").click();
+        $("#state").click();
+        $("#state").$(byText(state)).click();
+        $("#city").click();
+        $("#city").$(byText(city)).click();
+        $("#submit").click();
 
-    $(".table-responsive").shouldHave(text("Label Values"));
-    $(".table-responsive").shouldHave(text("Student Name" +  " " + firstName + " " + lastName));
-    $(".table-responsive").shouldHave(text("Student Email" + " " + email));
-    $(".table-responsive").shouldHave(text("Gender" + " " + gender));
-    $(".table-responsive").shouldHave(text("Mobile" + " " + mobile));
-    $(".table-responsive").shouldHave(text("Date of Birth" + " " + dayOfBirth+ " " + monthOfBirth + "," + yearOfBirth));
-    $(".table-responsive").shouldHave(text("Subjects" + " " + subject1 + ", " + subject2));
-    $(".table-responsive").shouldHave(text("Hobbies" + " " + hobby1 + ", " + hobby2 + ", " + hobby3));
-    $(".table-responsive").shouldHave(text("Picture" + " " + picture));
-    $(".table-responsive").shouldHave(text("Address" + " " + currentAddress));
-    $(".table-responsive").shouldHave(text("State and City" + " " + state + " " + city));
+        $(".table-responsive").shouldHave(text("Label Values"));
+        $(".table-responsive").shouldHave(text("Student Name" +  " " + firstName + " " + lastName));
+        $(".table-responsive").shouldHave(text("Student Email" + " " + email));
+        $(".table-responsive").shouldHave(text("Gender" + " " + gender));
+        $(".table-responsive").shouldHave(text("Mobile" + " " + mobile));
+        $(".table-responsive").shouldHave(text("Date of Birth" + " " + dayOfBirth+ " " + monthOfBirth + "," + yearOfBirth));
+        $(".table-responsive").shouldHave(text("Subjects" + " " + subject1 + ", " + subject2));
+        $(".table-responsive").shouldHave(text("Hobbies" + " " + hobby1 + ", " + hobby2 + ", " + hobby3));
+        $(".table-responsive").shouldHave(text("Picture" + " " + picture));
+        $(".table-responsive").shouldHave(text("Address" + " " + StateCurrentAddress + CityCurrentAddress));
+        $(".table-responsive").shouldHave(text("State and City" + " " + state + " " + city));
 
 
 
